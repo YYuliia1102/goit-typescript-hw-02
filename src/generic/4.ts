d0caec46ca203ff6b40b8f4f860cc86bce53def9
YYuliia1102 <reya1102@gmail.com>
2023-11-14 22:24:56 +0100

    fixed bags

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,23 +1,16 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-interface T {
+interface ComponentProps {
   title: string
 }
-class Component {
-
-  constructor(public props: T) {
-    this.props = props;
-  }
+class Component<T extends ComponentProps> {
+  constructor(public props: T) { }
 }
-class Page extends Component {
-
+class Page extends Component<{ title: string }> {
   pageInfo() {
     console.log(this.props.title);
   }
 }
-
-const page = new Page({ title: 'Home Page' });
 
 export { };
